c01bdcafc497ae3d4751d9e5eaa758dc8760e80d
NeoV0id <neo@LAPTOP-M3VPT85L.localdomain>
2022-03-23 00:51:38 +0100

    Added tree_height in 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,31 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - height of a binary tree
+ * @tree: pointer to the root node of the treee to measure the height
+ *
+ * Return: Height of the binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0, r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		l = 1 + binary_tree_height(tree->left);
+
+	if (tree->right != NULL)
+		r = 1 + binary_tree_height(tree->right);
+
+	if (l > r)
+		return (l);
+
+	return (r);
+}
+
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  *
