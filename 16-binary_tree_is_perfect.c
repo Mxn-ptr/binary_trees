ba403df8f7043b0eac327a41227ee7d1517c8437
NeoV0id <neo@LAPTOP-M3VPT85L.localdomain>
2022-03-23 11:50:07 +0100

    Modified task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -40,16 +40,18 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (height == 0)
+	else if (height == 0)
 		return (1);
-	if (tree->left == NULL || tree->right == NULL)
-		    return (0);
-	else if (tree->left == NULL && tree->right == NULL)
+
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+	else if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
 	else if (height > 0) 
 		if ((heightRight == heightLeft) && (heightRight == height -1))
 			if (tree->left != tree->right)
+				return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-
+	return (1);
 }
