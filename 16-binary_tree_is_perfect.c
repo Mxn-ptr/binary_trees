031a64ffc65ddf662b7ac8acdcc14c53878e94f3
NeoV0id <neo@LAPTOP-M3VPT85L.localdomain>
2022-03-23 11:38:10 +0100

    Added condition in 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -39,9 +39,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (height == 0)
-		return (0);
+		return (1);
 	if (tree->left == NULL || tree->right == NULL)
-		    return (1);
+		    return (0);
+	else if (tree->left == NULL && tree->right == NULL)
+		return (1);
 	else if ((height > 0) && (height == height -1))
 		if (tree->left != tree->right)
 			return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
