94ee7532a1d8bb93ca8e4fbaade1b7d561e541c7
LDoualito <3242@holbertonschool.com>
2022-03-24 11:38:44 +0100

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -42,5 +42,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,const binary_tree_t *second);
 
 #endif /* _BINARY_TREES_H_ */
